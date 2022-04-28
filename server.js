43634e2ead0ca872989b7ec226a3ac1887591640
Paul Butler <paul@driftingin.space>
2022-04-28 15:34:06 +0000

    echo environment variables

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const http = require('http');
 
-const hostname = '0.0.0.0';
-const port = 8080;
+const HOSTNAME = '0.0.0.0';
+const port = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader('Content-Type', 'application/json');
+
+  let response = {
+    message: "Hello World",
+    port: process.env.PORT || null,
+    spawner_service: process.env.SPAWNER_SERVICE || null,
+    spawner_url: process.env.SPAWNER_URL || null,
+    spawner_name: process.env.SPAWNER_NAME || null,
+  };  
+
+  res.end(JSON.stringify(response));
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${port}/`);
 });
 
